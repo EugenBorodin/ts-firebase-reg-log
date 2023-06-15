883fecbf357342f579cee36acd85819ab49b5ddb
Eugen Borodin <Azsalatay@gmail.com>
2023-06-15 19:42:05 +0200

    123

---
+++ src/app/App.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import AuthContext from '../contexts/AuthContext';
+import Register from '../components/Register/Register';
+import Profile from '../components/Profile/Profile';
+import { auth } from '../api/firebase';
+import UserContext from '../contexts/UserContext';
+import Login from '../components/Login/Login';
+import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
+
+function App(): JSX.Element {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+  }, []);
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <AuthContext.Provider value={currentUser}>
+      <UserContext.Provider value="7018054411">
+
+      <Routes>
+        <Route path="/" element={<ProtectedRoute outlet={<Profile />} />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+      </UserContext.Provider>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/contexts/UserContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const UserContext = createContext <string>('12345678');
+
+export default UserContext;
