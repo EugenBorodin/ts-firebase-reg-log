453049a3da8445521504e5fd1a9d4d70d1d562c0
Eugen Borodin <Azsalatay@gmail.com>
2023-06-16 17:23:01 +0200

    proekt

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {
   DocumentData, addDoc, collection,
+  deleteDoc,
+  doc,
   onSnapshot, orderBy, query, serverTimestamp,
 } from 'firebase/firestore';
 import { db } from '../../api/firebase';
@@ -12,9 +14,9 @@ export default function Todos(): JSX.Element {
   const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
   useEffect(() => {
     onSnapshot(q, (snapshot) => {
-      setTodos(snapshot.docs.map((doc: DocumentData) => ({
-        id: doc.id,
-        item: doc.data()
+      setTodos(snapshot.docs.map((document: DocumentData) => ({
+        id: document.id,
+        item: document.data()
       })));
     });
   }, [input, q]);
@@ -44,6 +46,14 @@ export default function Todos(): JSX.Element {
       {todos.map((el) => (
         <div key={el.id}>
           {el.item.todo}
+          <button
+            type="button"
+            onClick={
+              () => { deleteDoc(doc(db, 'todos', el.id)); }
+            }
+          >
+            Delete
+          </button>
         </div>
       ))}
 
